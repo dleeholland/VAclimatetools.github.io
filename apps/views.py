3963158a8fbf01cf9c9741f3538d652e35683a7a
D-Schecht <91809730+D-Schecht@users.noreply.github.com>
2023-07-24 21:16:21 -0400

    updates

--- apps/views.py
+++ apps/views.py
@@ -101,6 +101,9 @@ weather_data.loc[ (weather_data['MIN_TEMP']==9999.9) & (weather_data['MAX_TEMP']
 # Where MIN/AVG are known but MAX is not....calculate MAX
 weather_data.loc[ (weather_data['MIN_TEMP']!=9999.9) & (weather_data['MAX_TEMP'] == 9999.9),['MAX_TEMP']] = (weather_data['AVG_TEMP']*2)-weather_data['MIN_TEMP']
 
+# Where Prcp = 99.99 set to 0
+weather_data.loc[ (weather_data['Prcp']==99.99) ,['Prcp']] = 0
+
 #weather_data['DATE'] = pd.to_datetime(weather_data['DATE'])
 desc_cols = ['Station_ID',
              'Station_Name',
@@ -267,6 +270,14 @@ def display_temp():
         end_date_filter = request.form.get("temp-end2")
         max_heat_threshold_filter = request.form.get("max-heat-options")
         min_heat_threshold_filter = request.form.get("min-heat-options")
+        #colorblind_filter = request.form.getlist('colorblind_check')
+        colorblind_filter=False
+
+        if colorblind_filter == True:
+            chartcolors = ["rgba(73,82,91)","rgba(255, 0, 0, 0.4)","rgba(2,117,216,1)"]
+        else:
+            chartcolors = ["rgba(73,82,91)","rgba(255, 0, 0, 0.4)","rgba(2,117,216,1)"]
+
         
         
         #df = df[df['Station_Name'].isin(location_filter)]
@@ -456,9 +467,247 @@ def download_temp():
 # App main route + generic routing
 @app.route('/precipitation_analysis', methods=['GET','POST'])
 def display_precip():
+    prcp_threshold = list(range(0,20))
+    
+    if request.method == 'GET':
+        df = cache.get("weather_data")
+
+        if all(col in df.columns for col in cache.get("temperature_columns")):
+            df = df.drop(cache.get("temperature_columns"),axis=1)
+        else:
+            df = df
+        
+        cache.set("view_rain_data",df)
+        df_values = df.values
+        labels = [row for row in df.columns]
+        num_columns = df.shape[1]
+
+        stations = sorted(df['Station_Name'].unique())
+        counties = sorted(df['county'].unique())
+        max_date = str(df['DATE'].max().strftime("%Y-%m-%d")) 
+        min_date = str(df['DATE'].min().strftime("%Y-%m-%d"))
+
+        num_stations = len(stations)
+        
+
+        # Daily Line Chart Data
+        date_range = [np.datetime64(date) for date in sorted(df['DATE'].unique())]
+        date_range = pd.to_datetime(date_range)
+        date_range = [str(value.date()) for value in date_range]
+
+        temp_data_clean = df.dropna(subset=['Prcp'])
+        df_values_avg = temp_data_clean[['DATE','Prcp']].groupby("DATE", as_index=False).mean()
+        df_values_max = temp_data_clean[['DATE',"Prcp"]].groupby("DATE", as_index=False).max()
+        df_values_min = temp_data_clean[['DATE',"Prcp"]].groupby("DATE", as_index=False).min()
+
+        df_values_avg = df_values_avg.sort_values(by=['DATE'], ascending=[True])
+        df_values_max = df_values_max.sort_values(by=['DATE'], ascending=[True])
+        df_values_min = df_values_min.sort_values(by=['DATE'], ascending=[True])
+
+        max_prcp_threshold_filter = 10
+        min_prcp_threshold_filter = 0
+        start_date_filter = min_date
+        end_date_filter = max_date
+
+        # Get takeaway metrics for precipitation
+
+        df_values_max_threshold = df_values_max.loc[df_values_max['Prcp']>=int(max_prcp_threshold_filter)]
+        num_days_max_prcp_threshold = len(df_values_max_threshold['DATE'])
+        pct_days_max_prcp_threshold = np.round((num_days_max_prcp_threshold/len(df_values_max['Prcp']))*100,0)
+
+        df_values_min_threshold=df_values_min.loc[df_values_min['Prcp']>=int(min_prcp_threshold_filter)]
+        num_days_min_prcp_threshold = len(df_values_min_threshold['DATE'])
+        pct_days_min_prcp_threshold = np.round((num_days_min_prcp_threshold/len(df_values_min['Prcp']))*100,0)
+
+        num_days_minmax_prcp_threshold= len(df_values_min_threshold.merge(df_values_max_threshold, on=['DATE'], how='inner')['DATE'])
+        pct_days_minmax_prcp_threshold = np.round((num_days_minmax_prcp_threshold/len(df_values_min['Prcp']))*100,0)
+        
+
+        df_values_avg = [value for value in df_values_avg['Prcp']]
+        df_values_max = [value for value in df_values_max['Prcp']]
+        df_values_min = [value for value in df_values_min['Prcp']]
+
+        prcp_max_axis = np.round(max(df_values_max) *1.1,0)
+        prcp_min_axis =  0 if  min(df_values_min)>0 else np.round(min(df_values_min)* 1.1,0)
+
+
+        ##### Create Monthly Line Chart Data
+        df_values_avg_monthly = temp_data_clean[['MONTH','year','DATE','Prcp']].groupby(['MONTH','year'], as_index=False).mean()
+        df_values_max_monthly = temp_data_clean[['MONTH','year','DATE',"Prcp"]].groupby(['MONTH','year'], as_index=False).max()
+        df_values_min_monthly = temp_data_clean[['MONTH','year','DATE',"Prcp"]].groupby(['MONTH','year'], as_index=False).min()
+
+        df_values_avg_monthly = df_values_avg_monthly.sort_values(by=['year','MONTH'], ascending=[True,True])
+        df_values_max_monthly = df_values_max_monthly.sort_values(by=['year','MONTH'], ascending=[True,True])
+        df_values_min_monthly = df_values_min_monthly.sort_values(by=['year','MONTH'], ascending=[True,True])
+
+        df_values_avg_monthly = [value for value in df_values_avg_monthly['Prcp']]
+        df_values_max_monthly = [value for value in df_values_max_monthly['Prcp']]
+        df_values_min_monthly = [value for value in df_values_min_monthly['Prcp']]
+
+        date_range_monthly = df['DATE'].dt.strftime("%m/%y").unique().tolist()
+       # date_range_monthly = df['DATE'].dt.unique().tolist()
+
+
+       ###### Create BoxPlot
+        dfl = pd.melt(df, id_vars='MONTH_Name', value_vars=["Prcp"])
+        plt.clf()
+        colors = ["#0275D8"]
+        sb.set_palette(sb.color_palette(colors))
+        plot = sb.boxplot(x='MONTH_Name', y='value', data=dfl, showfliers=False, hue='variable')
+        #plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0)
+        plt.legend([], frameon=False)
+              
+        #set axis names
+        plot.set(xlabel='Month',
+                 ylabel='Precipitation')
+        
+        #boxplot_url = str(os.path.join(app.root_path,'static','assets','img','temp_box.png'))
+        plt.savefig(os.path.join(app.root_path, 'static','assets','img','prcp_box.png'))
+        #plt.savefig(boxplot_url)
+        boxplot_url = '/static/assets/img/prcp_box.png'
+
+        # Create Map Data
+
+           
+
+
+        ##### Render Template
+        
+        return render_template('home/precipitation_analysis.html',max_date=max_date,min_date=min_date, df_values=df_values, labels=labels,
+                               num_columns=num_columns, stations = stations, counties = counties,date_range = date_range, prcp_max_axis = prcp_max_axis,prcp_min_axis = prcp_min_axis,
+                               df_values_avg=df_values_avg,df_values_max=df_values_max,df_values_min=df_values_min,
+                               df_values_avg_monthly = df_values_avg_monthly, df_values_max_monthly=df_values_max_monthly,df_values_min_monthly=df_values_min_monthly,
+                               date_range_monthly=date_range_monthly,prcp_threshold=prcp_threshold, boxplot_name = boxplot_url ,boxplot_url = boxplot_url,
+                               num_days_max_prcp_threshold=num_days_max_prcp_threshold,num_days_min_prcp_threshold=num_days_min_prcp_threshold,
+                               pct_days_max_prcp_threshold=pct_days_max_prcp_threshold,pct_days_min_prcp_threshold=pct_days_min_prcp_threshold,max_prcp_threshold_filter=max_prcp_threshold_filter,
+                               min_prcp_threshold_filter=min_prcp_threshold_filter,num_days_minmax_prcp_threshold=num_days_minmax_prcp_threshold,
+                               pct_days_minmax_prcp_threshold=pct_days_minmax_prcp_threshold,start_date_filter=start_date_filter,end_date_filter=end_date_filter,
+                               num_stations=num_stations)
+     
+    if request.method == 'POST':
+
+        df = cache.get("weather_data")
+        if all(col in df.columns for col in cache.get("temperature_columns")):
+            df = df.drop(cache.get("temperature_columns"),axis=1)
+        else:
+            df = df
+
+        stations = sorted(df['Station_Name'].unique())
+        counties = sorted(df['county'].unique())
+        max_date = str(df['DATE'].max().strftime("%Y-%m-%d")) 
+        min_date = str(df['DATE'].min().strftime("%Y-%m-%d"))
+
+        #location_filter = request.form.getlist('Station_Select2')
+        county_filter = request.form.getlist('County_Select')
+        start_date_filter = request.form.get('prcp-start2')
+        end_date_filter = request.form.get("prcp-end2")
+        max_prcp_threshold_filter = request.form.get("max-prcp-options")
+        min_prcp_threshold_filter = request.form.get("min-prcp-options")
+        
+        
+        #df = df[df['Station_Name'].isin(location_filter)]
+        df = df[df['county'].isin(county_filter)]
+
+        num_stations = len(df['Station_Name'].unique())
+
+        if len(df[df["DATE"]<=pd.to_datetime(end_date_filter)]) == 0:
+            df = df.append(df.head(1),ignore_index=True)
+            df.loc[len(df)-1,['DATE','year','MONTH','DAY','Prcp']] = [pd.to_datetime(end_date_filter),np.NaN,np.NaN,np.NaN,np.Nan]
+
+        df = df[(df['DATE']<=end_date_filter) & (df['DATE']>=start_date_filter) ]
+
+        cache.set("view_rain_data",df)
+
+        df_values = df.values
+        labels = [row for row in df.columns]
+        num_columns = df.shape[1]    
+
+        date_range = [np.datetime64(date) for date in sorted(df['DATE'].unique())]
+        date_range = pd.to_datetime(date_range)
+        date_range = [str(value.date()) for value in date_range]
+
+        prcp_data_clean = df.dropna(subset=['Prcp'])
+        df_values_avg = prcp_data_clean[['DATE','Prcp']].groupby("DATE", as_index=False).mean()
+        df_values_max = prcp_data_clean[['DATE','Prcp']].groupby("DATE", as_index=False).max()
+        df_values_min = prcp_data_clean[['DATE','Prcp']].groupby("DATE", as_index=False).min()
+
+        df_values_avg = df_values_avg.sort_values(by=['DATE'], ascending=[True])
+        df_values_max = df_values_max.sort_values(by=['DATE'], ascending=[True])
+        df_values_min = df_values_min.sort_values(by=['DATE'], ascending=[True])
+
+        # Get takeaway metrics for precipitation
+
+        df_values_max_threshold = df_values_max.loc[df_values_max['Prcp']>=int(max_prcp_threshold_filter)]
+        num_days_max_prcp_threshold = len(df_values_max_threshold['DATE'])
+        pct_days_max_prcp_threshold = np.round((num_days_max_prcp_threshold/len(df_values_max['Prcp']))*100,0)
+
+        df_values_min_threshold=df_values_min.loc[df_values_min['Prcp']>=int(min_prcp_threshold_filter)]
+        num_days_min_prcp_threshold = len(df_values_min_threshold['DATE'])
+        pct_days_min_prcp_threshold = np.round((num_days_min_prcp_threshold/len(df_values_min['Prcp']))*100,0)
+
+        num_days_minmax_prcp_threshold= len(df_values_min_threshold.merge(df_values_max_threshold, on=['DATE'], how='inner')['DATE'])
+        pct_days_minmax_prcp_threshold = np.round((num_days_minmax_prcp_threshold/len(df_values_min['Prcp']))*100,0)
+
+        
+
+        df_values_avg = [value for value in df_values_avg['Prcp']]
+        df_values_max = [value for value in df_values_max['Prcp']]
+        df_values_min = [value for value in df_values_min['Prcp']]
+
+        prcp_max_axis = np.round(max(df_values_max) *1.1,0)
+        prcp_min_axis =  0 if  min(df_values_min)>0 else np.round(min(df_values_min)* 1.1,0)
+        
+        
+
+        ##### Create Monthly Line Chart Data
+        df_values_avg_monthly = prcp_data_clean[['MONTH','year','DATE','Prcp']].groupby(['MONTH','year'], as_index=False).mean()
+        df_values_max_monthly = prcp_data_clean[['MONTH','year','DATE',"Prcp"]].groupby(['MONTH','year'], as_index=False).max()
+        df_values_min_monthly = prcp_data_clean[['MONTH','year','DATE',"Prcp"]].groupby(['MONTH','year'], as_index=False).min()
+
+        df_values_avg_monthly = df_values_avg_monthly.sort_values(by=['year','MONTH'], ascending=[True,True])
+        df_values_max_monthly = df_values_max_monthly.sort_values(by=['year','MONTH'], ascending=[True,True])
+        df_values_min_monthly = df_values_min_monthly.sort_values(by=['year','MONTH'], ascending=[True,True])
+
+        df_values_avg_monthly = [value for value in df_values_avg_monthly['Prcp']]
+        df_values_max_monthly = [value for value in df_values_max_monthly['Prcp']]
+        df_values_min_monthly = [value for value in df_values_min_monthly['Prcp']]
+
+        date_range_monthly = df['DATE'].dt.strftime("%m/%y").unique().tolist()
+
+        ###### Create BoxPlot
+        dfl = pd.melt(df, id_vars='MONTH_Name', value_vars=["Prcp"])
+        plt.clf()
+        colors = ["#0275D8"]
+        sb.set_palette(sb.color_palette(colors))
+        plot = sb.boxplot(x='MONTH_Name', y='value', data=dfl, showfliers=False,hue="variable")
+        
+        #plot = sb.boxplot(x='MONTH', y='value', data=dfl, showfliers=False, hue='variable')
+        #plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0)
+        plt.legend([], frameon=False)
+              
+        #set axis names
+        plot.set(xlabel='Month',
+                 ylabel='Precipitation')
+        #boxplot_url = str(os.path.join(app.root_path,'static','assets','img','temp_box.png'))
+        plt.savefig(os.path.join(app.root_path, 'static','assets','img','prcp_box.png'))
+        #plt.savefig(boxplot_url)
+        boxplot_url = '/static/assets/img/prcp_box.png'
+
+        ##### Render Template
+        
+        return render_template('home/precipitation_analysis.html',max_date=max_date,min_date=min_date, df_values=df_values, labels=labels,
+                               num_columns=num_columns, stations = stations, counties = counties,date_range = date_range, prcp_max_axis = prcp_max_axis,prcp_min_axis = prcp_min_axis,
+                               df_values_avg=df_values_avg,df_values_max=df_values_max,df_values_min=df_values_min,
+                               df_values_avg_monthly = df_values_avg_monthly, df_values_max_monthly=df_values_max_monthly,df_values_min_monthly=df_values_min_monthly,
+                               date_range_monthly=date_range_monthly,prcp_threshold=prcp_threshold, boxplot_name = boxplot_url ,boxplot_url = boxplot_url,
+                               num_days_max_prcp_threshold=num_days_max_prcp_threshold,num_days_min_prcp_threshold=num_days_min_prcp_threshold,
+                               pct_days_max_prcp_threshold=pct_days_max_prcp_threshold,pct_days_min_prcp_threshold=pct_days_min_prcp_threshold,max_prcp_threshold_filter=max_prcp_threshold_filter,
+                               min_prcp_threshold_filter=min_prcp_threshold_filter,num_days_minmax_prcp_threshold=num_days_minmax_prcp_threshold,
+                               pct_days_minmax_prcp_threshold=pct_days_minmax_prcp_threshold,start_date_filter=start_date_filter,end_date_filter=end_date_filter,
+                               num_stations=num_stations)
     
 
-    return render_template('home/precipitation_analysis.html')
+    #return render_template('home/precipitation_analysis.html')
 
 # App main route + generic routing
 @app.route('/precipitation_table', methods = ['GET','POST'])
